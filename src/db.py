44bd0b8efe0a98350ffe3cc1592b9ff65ad348d0
Alexp163 <dr.p163@ya.ru>
2023-10-20 10:05:59 +0400

    Подключение БД в папке db.py и прописывание моделей в папке models.py

--- src/db.py
+++ src/db.py
@@ -1,2 +1,6 @@
-if __name__ == '__main__':
-    main()
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+
+app.config["SQLALCHEMY_DATABASE_URL"] = "sqlite:///slite.db"
+db = SQLAlchemy(app)
--- src/models.py
+++ src/models.py
@@ -1,2 +1,27 @@
-if __name__ == '__main__':
-    main()
+from db import db
+
+
+# id - число
+# name_product - наименование продукта, услуги
+# technology - технология изготовления
+# date_development - дата изготовления
+# price - стоимость
+# developer - разработчик
+# ______________________________
+# db.Strings(50) - текстовое поле заданной длины
+# db.Integer() - числовое поле
+# db.Column() - создание колонки(дополнительные поля: nullable-не может быть пустой, unique - уникальна, primary_key)
+# для id всегда задается primary_key=True
+
+
+class Development:
+    __tablename__ = "development"
+
+    id = db.Column(db.Integer(), primary_key=True) # идентификационный номер
+    name_product = db.Column(db.String()) # наименование продукта(услуги)
+
+    technology = db.Column(db.String())
+    date_development = db.Column(db.String(10))
+
+    price = db.Column(db.String(10))
+    developer = db.Column(db.String())
