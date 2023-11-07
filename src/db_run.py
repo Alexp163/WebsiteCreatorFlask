f10c48373a619f93ecc4e95c5c6cb6bb00890e62
Alexp163 <dr.p163@ya.ru>
2023-11-07 10:51:13 +0400

    Коррекция sqlite.db,  создание ServiceGroup

--- src/db_run.py
+++ src/db_run.py
@@ -1,26 +1,56 @@
 from db import db
 from app import app
-from models import Development
-
+from models import Development, Service, ServiceGroup
 
 with app.app_context():
     db.drop_all()
     db.create_all()
 
-
 with app.app_context():
-    development = Development(name_product = "разработка бэкэнда", technology = "flask",
-                              date_development = "10.10.2023", price = "50000.00",
-                              developer = "Александр Попов")
+    development = Development(name_product="разработка бэкэнда",
+                              technology="flask",
+                              date_development="10.10.2023", price="50000.00",
+                              developer="Александр Попов")
     db.session.add(development)
-    development1 = Development(name_product = "разработка дизайна сайта", technology = "Photoshop",
-                              date_development = "12.10.2023", price = "10000.00",
-                              developer = "Сергей Трофимов")
+    development1 = Development(name_product="разработка дизайна сайта",
+                               technology="Photoshop",
+                               date_development="12.10.2023", price="10000.00",
+                               developer="Сергей Трофимов")
     db.session.add(development1)
     db.session.commit()
 
-
 with app.app_context():
-    development = Development.query.all()
-    for d in development:
-        print(d.name_product, d.price, d.developer)
+    group1 = ServiceGroup(name="Разработка дизайна сайта",
+                          description="Здесь будет описана группа")
+    group2 = ServiceGroup(name="Верстка страниц сайта",
+                          description="Здесь будет описана группа")
+    group3 = ServiceGroup(name="Разработка фронтенда",
+                          description="Здесь будет описана группа")
+    group4 = ServiceGroup(name="Разработка бэкэнда",
+                          description="Здесь будет описана группа")
+    db.session.add(group1)
+    db.session.add(group2)
+    db.session.add(group3)
+    db.session.add(group4)
+    db.session.commit()
+    service = Service(name_product="Разработка дизайна сайта",
+                      technоlogi="Photoshop", date_development="7 дней",
+                      price="10000.0",
+                      developer="Веб-дизайнер Иващук И.В")
+    db.session.add(service)
+    service1 = Service(name_product="Верстка страниц сайта",
+                       technоlogi="HTML, CSS, Bootstrap",
+                       date_development="10 дней", price="15000.0",
+                       developer="Верстальщик Сергеев В.Ф.")
+    db.session.add(service1)
+    service2 = Service(name_product="Разработка фронтенда сайта",
+                       technоlogi="Javascript, HTML, CSS",
+                       date_development="14 дней", price="20000.0",
+                       developer="Фронтенд-разработчик Сурков А.С.")
+    db.session.add(service2)
+    service3 = Service(name_product="Разработка бэкэнда сайта",
+                       technоlogi="Python, flask", date_development="30 дней",
+                       price="30000.0",
+                       developer="Бэкэнд разработчик Паштиян Х.А.")
+    db.session.add(service3)
+    db.session.commit()
--- src/models.py
+++ src/models.py
@@ -13,11 +13,32 @@ from db import db
 class Development(db.Model):
     __tablename__ = "development"
 
-    id = db.Column(db.Integer(), primary_key=True) # идентификационный номер
-    name_product = db.Column(db.String(20)) # наименование продукта(услуги)
+    id = db.Column(db.Integer(), primary_key=True)  # идентификационный номер
+    name_product = db.Column(db.String(20))  # наименование продукта(услуги)
 
-    technology = db.Column(db.Text()) # технология выполнения
-    date_development = db.Column(db.String(10)) # дата разработки продукта
+    technology = db.Column(db.Text())  # технология выполнения
+    date_development = db.Column(db.String(10))  # дата разработки продукта
 
-    price = db.Column(db.Float()) # цена продукта
-    developer = db.Column(db.String(20)) # разработчик
+    price = db.Column(db.Float())  # цена продукта
+    developer = db.Column(db.String(20))  # разработчик
+
+
+class Service(db.Model):
+    __tablename__ = 'service'
+    id = db.Column(db.Integer(), primary_kay=True)
+    name_product = db.Column(db.String(20))  # наименование продукта
+
+    technology = db.Column(db.Text())  # технология выполнения
+    date_development = db.Column(db.String(10))  # дата изготовления
+
+    price = db.Column(db.Float())  # цена продукта
+    developer = db.Column(db.String(20))  # разработчик
+
+
+class ServiceGroup(db.Model):
+    __tablename__ = "service_group"
+    id = db.Column(db.Integer(), primary_key=True)
+
+    name = db.Column(db.String(100))  # название группы
+    description = db.Column(db.Text())  # описание группы
+    # создать руководителя группы
