2790fe371c19474df0ecb0c8e5686267d5f90a2a
Alexp163 <dr.p163@ya.ru>
2023-11-28 19:05:20 +0400

    Обработка форм 1. Установить flask-wtf для работы с формами 2. Создать два класса LoginForm и RegisterForm 3. Пропихнуть эти формы на странички с авторизацией и регистрацией 4. Джинджей обработать формы на страничке 5. Не забыть добавить кнопку submit

--- src/forms.py
+++ src/forms.py
@@ -1,2 +1,16 @@
-if __name__ == '__main__':
-    main()
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
+
+class LoginForm(FlaskForm):
+    email = EmailField("Почта", validators=[DataRequired(), Email()])
+    password = PasswordField("Пароль", validators=[DataRequired(), Length(8, 32)])
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField("Почта", validators=[DataRequired(), Email()])
+    password_1 = PasswordField("Пароль", validators=[DataRequired(), Length(8, 32)])
+    password_2 = PasswordField("Пароль", validators=[DataRequired(), Length(8, 32)])
+
+    def are_password_equal(self):
+        pass
--- src/view.py
+++ src/view.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 from models import Development, Service, ServiceGroup
+from forms import LoginForm, RegisterForm
 
 
 @app.route('/')
@@ -17,12 +18,14 @@ def our_projects():
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 
 @app.route('/registration')
 def registration():
-    return render_template('registration.html')
+    form = RegisterForm()
+    return render_template('registration.html', form=form)
 
 
 @app.route('/work/1')
